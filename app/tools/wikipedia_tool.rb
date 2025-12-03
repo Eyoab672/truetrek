7abb63449bc3ff383d8128653aacefc1f8212810
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-12-03 16:33:02 +0100

    added tool

---
+++ app/tools/wikipedia_tool.rb
@@ -0,0 +1,7 @@
+require 'open-uri'
+
+class WikipediaTool < RubyLLM::Tool
+  description "Gets a summary of the place"
+  param :title "title given by the user for the place"
+  param :title "title given by the user for the place"
+end
