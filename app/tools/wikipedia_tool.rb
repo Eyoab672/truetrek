e9fe4d9ff0b88d76e019010c38158f4a9ab1158c
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-12-08 11:49:58 +0100

    Fix: Refine error handling in WikipediaTool by specifying StandardError rescue

--- app/tools/wikipedia_tool.rb
+++ app/tools/wikipedia_tool.rb
@@ -2,7 +2,6 @@ require 'open-uri'
 class WikipediaTool < RubyLLM::Tool
   description "Gets a summary of the place"
   param :place_name, desc: "name of the place"
-  
 
   def execute(place_name:)
     url = "https://en.wikipedia.org/api/rest_v1/page/summary/#{place_name}"
@@ -11,7 +10,7 @@ class WikipediaTool < RubyLLM::Tool
     data = JSON.parse(response)
 
     { success: true, data: data }
-  rescue => e
+  rescue StandardError => e
     { success: false, reason: "wikipedia_unavailable", error: e.message }
   end
 end
